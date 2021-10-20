2cf1232a7b0dc2d3f0716609b30fd3788521b221
Kaamyak Pant <kaamyak@local.com>
2021-10-20 17:53:05 +0530

    added environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 process.env.DEBUG = 'node';
 const express = require('express');
 const products = require('./data/products');
+const dotenv = require('dotenv');
+
+dotenv.config()
 
 const app = express();
 
@@ -19,4 +22,6 @@ app.get('/api/products/:id', (req,res) => {
     
 })
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
