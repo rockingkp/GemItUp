6e75d69e7a876f1a5b2290f9024f56001a6babbd
Kaamyak Pant <kaamyak@local.com>
2021-10-20 20:34:11 +0530

    connected to MongoDB database using mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ process.env.DEBUG = 'node';
 import express from 'express';
 import dotenv from 'dotenv';
 import products from './data/products.js';
+import connectDB from './config/db.js';
 
 
 dotenv.config()
+
+connectDB();
 
 const app = express();
 
