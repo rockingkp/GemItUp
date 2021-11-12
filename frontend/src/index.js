dc06e596a5d3de71f8695b66643c9fc24c6f4ab6
Your Name <kaamyakpant@gmail.com>
2021-11-12 23:36:25 +0530

    update: setting up redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from 'react-redux'
+import store from './store.js'
 import './bootstrap.min.css';
 import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store= {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
