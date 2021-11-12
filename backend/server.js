8cbdff7d8f9738997aaa0146f96b9654954cdf2b
Your Name <kaamyakpant@gmail.com>
2021-11-12 16:46:23 +0530

    feat/fetching products from database added

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ process.env.DEBUG = 'node';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
-import products from './data/products.js';
 import connectDB from './config/db.js';
+import productRoutes from './routes/productRoutes.js'
 
 
 
@@ -17,16 +17,7 @@ app.get('/', (req,res) => {
     res.send('API is running...');
 })
 
-app.get('/api/products', (req,res) => {
-    res.json(products);
-    
-})
-
-app.get('/api/products/:id', (req,res) => {
-    const product = products.find(p => p._id === req.params.id);
-    res.json(product)
-    
-})
+app.use('/api/products', productRoutes)
 
 const PORT = process.env.PORT || 5000;
 
