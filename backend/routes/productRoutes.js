53120a5c7eed9aa32c02abcc7e3877f4a9c5453f
Your Name <kaamyakpant@gmail.com>
2021-11-13 17:48:38 +0530

    update: loader and error components

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,34 @@
-import express from "express"
+import express from "express";
 import asyncHandler from "express-async-handler";
-const router = express.Router()
-import Product from '../models/productModel.js';
-
+const router = express.Router();
+import Product from "../models/productModel.js";
 
 // @desc              FETCH ALL PRODUCTS
 //@route              GET /api/products
 //@access             Public
-router.get('/', asyncHandler(async (req,res) => {
-    const products = await Product.find({})
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    const products = await Product.find({});
     res.json(products);
-    
-}))
+  })
+);
 
 // @desc              FETCH SINGLE PRODUCT
 //@route              GET /api/products/:id
 //@access             Public
-router.get('/:id', asyncHandler(async(req,res) => {
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
 
-    if(product){
-        res.json(product)
-    } else{
-        res.status(404)
-        throw new Error('Product Not Found')
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error("Product Not Found");
     }
-
-    
-    
-}))
+  })
+);
 
 export default router;
