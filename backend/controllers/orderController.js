b8f3575c8204c30b2db8dbb88c47506701eb77e0
Your Name <kaamyakpant@gmail.com>
2021-11-21 19:28:54 +0530

    update: setting order details on user profile

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -77,4 +77,12 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+// @desc              GET LOGGED IN USER ORDERS
+//@route              GET /api/orders/myorders
+//@access             Private
+const getMyOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
