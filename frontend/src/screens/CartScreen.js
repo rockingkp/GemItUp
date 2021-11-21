891ac14b996ae1beabc507a186e5aa4b82380eda
Your Name <kaamyakpant@gmail.com>
2021-11-21 12:42:30 +0530

    update: creating orders functionality and minor bug fixes

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -112,7 +112,7 @@ const CartScreen = ({ match, location, history }) => {
             <ListGroup.Item className="m-auto">
               <Button
                 type="button"
-                className="btn-block m-auto"
+                className="w-100"
                 disabled={cartItems.length === 0}
                 onClick={checkoutHandler}
               >
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import {
   Form,
@@ -12,8 +12,10 @@ import {
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
 import CheckoutSteps from "../components/CheckoutSteps";
+import { createOrder } from "../actions/orderActions";
 
-const PlaceOrderScreen = () => {
+const PlaceOrderScreen = ({ history }) => {
+  const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart);
 
   //PRICING VARIABLES
@@ -36,8 +38,28 @@ const PlaceOrderScreen = () => {
     Number(cart.taxPrice)
   ).toFixed(2);
 
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { order, success, error } = orderCreate;
+
+  useEffect(() => {
+    if (success) {
+      history.push(`/order/${order._id}`);
+    }
+    // eslint-disable-next-line
+  }, [history, success]);
+
   const placeOrderHandler = () => {
-    console.log("order");
+    dispatch(
+      createOrder({
+        orderItems: cart.cartItems,
+        shippingAddress: cart.shippingAddress,
+        paymentMethod: cart.paymentMethod,
+        itemsPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
+        totalPrice: cart.totalPrice,
+      })
+    );
   };
   return (
     <>
@@ -129,6 +151,10 @@ const PlaceOrderScreen = () => {
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
+                <ListGroup.Item>
+                  {error && <Message variant="danger">{error}</Message>}
+                </ListGroup.Item>
+
                 <Button
                   type="button"
                   className="w-100"
