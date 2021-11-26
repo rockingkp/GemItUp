3502c6bcbae844df067b2133688e96e6e793aa28
Your Name <kaamyakpant@gmail.com>
2021-11-27 01:54:31 +0530

    update: user edit screen

--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -24,7 +24,7 @@ const UserListScreen = ({ history }) => {
     } else {
       history.push("/login");
     }
-  }, [dispatch, history, successDelete]);
+  }, [dispatch, history, successDelete, userInfo]);
 
   const deleteHandler = (id) => {
     if (window.confirm("Are You Sure?")) {
@@ -67,7 +67,7 @@ const UserListScreen = ({ history }) => {
                   )}
                 </td>
                 <td>
-                  <LinkContainer to={`/user/${user._id}/edit`}>
+                  <LinkContainer to={`/admin/user/${user._id}/edit`}>
                     <Button variant="dark" className="btn-sm">
                       <i className="fas fa-edit"></i>
                     </Button>
