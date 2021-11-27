7faca1cf09059440ecb0e9b616704a34115d3915
Your Name <kaamyakpant@gmail.com>
2021-11-27 22:10:01 +0530

    update: admin screen product edit screen

---
+++ frontend/src/screens/ProductEditScreen.js
@@ -0,0 +1,138 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+import { listProductDetails } from "../actions/productActions";
+
+const ProductEditScreen = ({ match, history }) => {
+  const productId = match.params.id;
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [carats, setCarats] = useState(0);
+  const [image, setImage] = useState("");
+  const [seller, setSeller] = useState("");
+  const [description, setDescription] = useState("");
+  const [countInStock, setCountInStock] = useState(0);
+
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
+
+  useEffect(() => {
+    if (!product.name || product._id !== productId) {
+      dispatch(listProductDetails(productId));
+    } else {
+      setName(product.name);
+      setPrice(product.price);
+      setCarats(product.carats);
+      setImage(product.image);
+      setSeller(product.seller);
+      setDescription(product.description);
+      setCountInStock(product.setCountInStock);
+    }
+  }, [dispatch, history, productId, product]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    //UPDATE PRODUCT
+  };
+
+  return (
+    <>
+      <Link to="/admin/productlist" className="btn btn-dark my-3">
+        Go Back
+      </Link>
+      <FormContainer>
+        <h1>EDIT PRODUCT</h1>
+
+        {loading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="danger">{error}</Message>
+        ) : (
+          <Form onSubmit={submitHandler}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="name"
+                placeholder="Enter Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="carats">
+              <Form.Label>Carats</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter Carats"
+                value={carats}
+                onChange={(e) => setCarats(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="price">
+              <Form.Label>Price</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter Price"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="image">
+              <Form.Label>Image</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Image URL"
+                value={image}
+                onChange={(e) => setImage(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="seller">
+              <Form.Label>Seller</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Seller"
+                value={image}
+                onChange={(e) => setSeller(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="countInStock">
+              <Form.Label>Count In Stock</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="Enter Count In Stock"
+                value={countInStock}
+                onChange={(e) => setCountInStock(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Form.Group controlId="description">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              ></Form.Control>
+            </Form.Group>
+
+            <Button type="submit" variant="primary" className="mt-4">
+              Update
+            </Button>
+          </Form>
+        )}
+      </FormContainer>
+    </>
+  );
+};
+
+export default ProductEditScreen;
